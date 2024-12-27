91632d7eabeb1a8578650d727beaaab9c0bbaef3
vectris-dev <steve@vectr.is>
2024-12-28 08:02:55 +1100

    Adding news API request

--- sketch.js
+++ sketch.js
@@ -1,8 +1,11 @@
 let theShader;
 let sentiment = 1.0;
+// Please don't steal my api key lol you can get a free one from https://thenewsapi.com/
+const API_ENDPOINT = 'https://api.thenewsapi.com/v1/news/top?api_token=o2nVs5g3POU9GlHFqfjIDEuQD9MfUMAGJbUSKl0q&locale=us&limit=3';
 
 function preload() {
     theShader = loadShader('shader.vert', 'shader.frag');
+    fetchNews();
 }
 
 function setup() {
@@ -26,4 +29,17 @@ function windowResized() {
 
 function updateSentiment(newSentiment) {
     sentiment = constrain(newSentiment, 0, 1);
+}
+
+async function fetchNews() {
+    try {
+        const response = await fetch(API_ENDPOINT);
+        const data = await response.json();
+        
+        data.data.forEach(article => {
+            console.log(article.description);
+        });
+    } catch (error) {
+        console.error('Error fetching news:', error);
+    }
 } 
